aed37b8763070dcc521cfcb7cbe3a4b8a533e01f
yangzhan <728074993@qq.com>
2016-09-23 17:54:20 +0800

    add Documentation

--- README.md
+++ README.md
@@ -26,3 +26,22 @@
 |GSA     |GNSS receiver operating mode, satellites used in the position solution, and DOP values| GP, GN|
 |GSV |Number of GNSS satellites in view satellite ID numbers, elevation, azimuth, & SNR values |GP,GL,GN|
 |RMC| Time, date, position, course and speed data| GP,GN|
+
+##返回的GPS数据
+单次 
+```
++CGNSINF: 1,1,20160921235709.000,30.439608,114.427183,14.900,0.65,201.0,1,,1.2,1.6,0.9,,14,4,,,37,,
+```
+
+连续 
+```
+$GPGLL,3026.3577,N,11425.6279,E,235943.000,A,A*5B
+$GPGSA,A,3,08,27,28,03,30,,,,,,,,2.68,1.36,2.31*0D
+$GPGSV,4,1,14,11,77,051,16,01,72,134,22,07,63,219,14,30,58,285,27*71
+$GPGSV,4,2,14,08,38,042,41,28,30,316,39,22,16,134,23,17,16,257,20*7E
+$GPGSV,4,3,14,03,08,153,31,27,06,060,23,16,03,110,21,09,02,205,*7A
+$GPGSV,4,4,14,13,01,313,,193,,,*75
+$GPRMC,235943.000,A,3026.3577,N,11425.6279,E,0.00,5.73,210916,,,A*60
+$GPVTG,5.73,T,,M,0.00,N,0.00,K,A*3C
+$GPGGA,235944.000,3026.3577,N,11425.6279,E,1,5,1.36,42.8,M,-
+```
--- test.py
+++ test.py
@@ -1,41 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+__author__ = 'yangzha'
 import serial,time,math
 
-modem = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+def getGPS():
+	"""
+	通过sim808获取串口返回的GPS坐标
+	"""
+	#modem = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) #树莓派
+	modem = serial.Serial('COM5',9600,timeout=1)
+	modem.write("AT+CGNSPWR=1\r")
+	modem.readline()
+	GPSStatus = modem.readline()
+	time.sleep(2)
 
-modem.write("AT+CGNSPWR=1\r")
-modem.readline()
-status = modem.readline()
-time.sleep(2)
-modem.write("AT+CGNSSEQ=\"RMC\"\r")
-modem.readline()
-status = modem.readline()
-time.sleep(2)
-modem.write("AT+CGNSTST=1\r")
-modem.readline()
-status = modem.readline()
-
-time.sleep(2)
-
-
-print "Starting loop..."
-while True:
-	try:
-		line = modem.readline()
-		if line.startswith("$GPGGA"):
-			index = line.split(",")
-			print index
-			tempNdata = index[2] / 100
-			tempEdata = index[4] / 100
-			Ndata = math.floor(tempNdata)
-			Edata = math.floor(tempEdata)
-			tempNdata = (tempNdata - Ndata) * 100
-			tempEdata = (tempEdata - Edata) * 100
-			Ndata += tempNdata / 60
-			Edata += tempEdata / 60
-			print ('Got it! N: %s and E: %s',(Ndata,Edata))
-		time.sleep(1)
-	except:
-		print "quitting"
-		modem.write("AT+CGNSTST=0\r")
-		modem.readline()
-		exit(0)
+	modem.write("AT+CGNSINF\r")
+	modem.readline()
+	data = modem.readline()
+	print data
+	if data.startswith("+CGNSINF"):
+		data = data.split(",")
+		if data[3]=='':
+			print "GPS未连接"
+			
+		else:
+			#Latitude ±dd.dddddd [-90.000000,90.000000]
+			Latitude = float(data[3])
+			#Longitude ±ddd.dddddd [-180.000000,180.000000]
+			Longitude = float(data[4])
+			print "Latitude=",Latitude,";Longitude=",Longitude
+			return (Latitude,Longitude)
+		modem.close()
+if __name__ == '__main__':
+	getGPS()
